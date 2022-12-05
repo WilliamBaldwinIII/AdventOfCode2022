e2c5f9a1666ce204cae6e37c2fe2ffc11b950f41
William Baldwin <9921967+WilliamBaldwinIII@users.noreply.github.com>
2022-12-05 08:07:56 -0700

    Day 5: Parsing stacks works

--- 5.fsx
+++ 5.fsx
@@ -17,7 +17,11 @@ let countIndex =
 let stacks, moves = lines |> List.splitAt (countIndex + 1)
 
 let stacksRev = stacks |> List.rev
-let (::) indexesLine, crateLines = stacksRev
+
+let indexesLine, crateLines =
+    match stacksRev with
+    | indexesLine :: crateLines -> indexesLine, crateLines
+    | _ -> failwith "Error! Something's wrong with the parsed stack lines"
 
 /// Map indexes to their programmatic indexes
 let indexMap =
@@ -50,15 +54,16 @@ let parsedCrates = crateLines |> Seq.map parseLine
 let indexValues = indexMap.Values |> seq
 
 let addLineToStacks (stackMap: Map<int, string list>) (currentLineCrateList: Map<int, string>) =
-    for i in indexValues do
+    let lineFold (stackMap': Map<int, string list>) i =
         match currentLineCrateList.TryFind i with
-        | None -> stackMap
+        | None -> stackMap'
         | Some crate ->
-            match stackMap.TryFind i with
-            | None -> stackMap.Add(i, [ crate ])
-            | Some crateList ->
-                stackMap.Add(i, crate::crateList)
+            let crate' = [ crate ]
 
-let crateStacks = parsedCrates |> Seq.fold
+            match stackMap'.TryFind i with
+            | None -> stackMap'.Add(i, crate')
+            | Some crateList -> stackMap'.Add(i, crateList @ crate')
 
-printfn $"{line2}"
+    indexValues |> Seq.fold lineFold stackMap
+
+let crateStacks = parsedCrates |> Seq.fold addLineToStacks Map.empty
