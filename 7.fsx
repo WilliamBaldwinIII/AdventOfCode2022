386b30ab7b297173661d6d0c967ba156ae47cb7f
William Baldwin <9921967+WilliamBaldwinIII@users.noreply.github.com>
2022-12-06 23:40:04 -0700

    7 - Brainstorming

---
+++ 7.fsx
@@ -0,0 +1,23 @@
+﻿#load "FileHelpers.fs"
+#load "Helpers.fs"
+
+open System
+open System.Text.RegularExpressions
+open Helpers
+
+
+let fileName = $"7"
+
+let lines = FileHelpers.readNumFile fileName
+
+type Command =
+    | List
+    | ChangeDir of string
+
+type File = { Name: string; Size: bigint }
+
+type Directory =
+    { Name: string
+      Files: File list
+      Directories: Directory list }
+//and FileType
