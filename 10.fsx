5391a34ae1078f25793a6991330a184b4becb94f
William Baldwin <9921967+WilliamBaldwinIII@users.noreply.github.com>
2022-12-11 15:20:46 -0700

    10-1 Setting up a lot

---
+++ 10.fsx
@@ -0,0 +1,58 @@
+﻿#load "FileHelpers.fs"
+#load "Helpers.fs"
+
+open Helpers
+open System
+
+//fsi.ShowDeclarationValues <- false
+
+let fileName = $"10-ex"
+
+let fileLines = FileHelpers.readNumFile fileName
+
+let maxCycle = 220
+
+let isCountCycle cycle = (cycle - 20) % 40 = 0
+
+type Move =
+    | Noop
+    | AddX of int
+
+let moveCycleCount =
+    function
+    | Noop -> 1
+    | AddX _ -> 2
+
+let parseMove (line: string) =
+    let lineArr = line |> String.split " "
+
+    match lineArr with
+    | [| "addx"; num |] -> num |> int |> AddX
+    | [| "noop" |] -> Noop
+    | other -> failwith $"Invalid move! {other}"
+
+let moves = fileLines |> List.map parseMove
+
+type Cycle =
+    { CurrentCycle: int
+      CurrentSignalStrengthSum: int
+      SignalStrenths: int list }
+
+let processMove (cycle: Cycle) (move: Move) = 
+        
+    let rec runCycle (numCyclesRemainingForMove: int) = 
+        let signalStrength = 
+            if isCountCycle cycle.CurrentCycle then cycle.CurrentSignalStrengthSum
+
+    cycle
+
+let startCycle =
+    { CurrentCycle = 0
+      CurrentSignalStrengthSum = 0
+      SignalStrenths = [] }
+
+let endCycle = moves |> List.fold processMove startCycle
+
+let signalStrengthSum = endCycle.SignalStrenths |> List.sum
+
+printfn $"Part 1: {signalStrengthSum}"
