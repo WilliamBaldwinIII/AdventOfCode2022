b63881dd98ebc371a1d139ad71c27db5a1e50a2e
William Baldwin <9921967+WilliamBaldwinIII@users.noreply.github.com>
2022-12-11 15:39:12 -0700

    10 -1 mostly done

--- 10.fsx
+++ 10.fsx
@@ -36,23 +36,48 @@ let moves = fileLines |> List.map parseMove
 type Cycle =
     { CurrentCycle: int
       CurrentSignalStrengthSum: int
-      SignalStrenths: int list }
+      SignalStrengths: int list }
 
-let processMove (cycle: Cycle) (move: Move) = 
-        
-    let rec runCycle (numCyclesRemainingForMove: int) = 
-        let signalStrength = 
-            if isCountCycle cycle.CurrentCycle then cycle.CurrentSignalStrengthSum
+let processMove (cycle: Cycle) (move: Move) =
 
-    cycle
+    let rec runCycle currentCycle (numCyclesRemainingForMove: int) signalStrengths =
+        if numCyclesRemainingForMove <= 0 then
+            currentCycle, signalStrengths
+        else
+            let signalStrength =
+                if isCountCycle cycle.CurrentCycle then
+                    Some cycle.CurrentSignalStrengthSum
+                else
+                    None
+
+            let signalStrengths =
+                signalStrength
+                |> Option.map (fun s -> s :: cycle.SignalStrengths)
+                |> Option.defaultValue cycle.SignalStrengths
+
+            runCycle (currentCycle + 1) (numCyclesRemainingForMove - 1) signalStrengths
+
+    let moveCycleCount = moveCycleCount move
+
+    let newCycle, signalStrengths =
+        runCycle cycle.CurrentCycle moveCycleCount cycle.SignalStrengths
+
+    let newTotal =
+        match move with
+        | Noop -> cycle.CurrentSignalStrengthSum
+        | AddX num -> cycle.CurrentSignalStrengthSum + num
+
+    { CurrentCycle = newCycle
+      CurrentSignalStrengthSum = newTotal
+      SignalStrengths = signalStrengths }
 
 let startCycle =
     { CurrentCycle = 0
       CurrentSignalStrengthSum = 0
-      SignalStrenths = [] }
+      SignalStrengths = [] }
 
 let endCycle = moves |> List.fold processMove startCycle
 
-let signalStrengthSum = endCycle.SignalStrenths |> List.sum
+let signalStrengthSum = endCycle.SignalStrengths |> List.sum
 
 printfn $"Part 1: {signalStrengthSum}"
